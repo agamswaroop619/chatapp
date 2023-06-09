ac85797d9572cde03fbad6b9cc5d805852b47dbc
agamswaroop619 <agam.swaroop619@gmail.com>
2023-06-09 15:14:19 +0530

    "navbar features added"

--- src/Components/ChatRoom/Sidnav.js
+++ src/Components/ChatRoom/Sidnav.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
-
+import Chat from './Chat';
 export class Sidnav extends Component {
   render() {
     return (
         <div className='w-1/6 h-19/20 left-0 fixed bg-gray-800 bottom-0'>
-            <chat></chat>
+            <Chat></Chat>
           {firebase.auth().currentUser && (
             <div>
               <button onClick={() => firebase.auth().signOut()} className='absolute inset-x-0 bottom-0 rounded-full p-2 border-whitesmoke text-xl border-4 border-solid bg-gray-800 text-white font-mono text-[20px] px-5 '>
--- src/Components/ChatRoom/Topnav.js
+++ src/Components/ChatRoom/Topnav.js
@@ -4,10 +4,10 @@ export class Topnav extends Component {
   render() {
     return (
         <div className='h-1/20 w-screen top-0 fixed bg-gray-800 font-mono'>
-            <div className='w-1/6 h-1/20 text-[30px] text-white select-none flexbox underline decoration-dashed'>   
-                ChatApp
+            <div className='w-1/6 h-1/20 text-[35px] text-white select-none underline decoration-dashed align-bottom py-4'>   
+                <p>ChatApp</p>
             </div>
-            <div>   
+            <div className='w-1/6 h-19/20 text-[30px] text-white select-none flexbox underline decoration-dashed'>    
             </div>
         </div>
     )
