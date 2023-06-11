5aa7ca2975e2c06c1f61108478f9e7edc1c61a60
agamswaroop619 <agam.swaroop619@gmail.com>
2023-06-12 02:18:14 +0530

    "bugs resolved"

--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -1,10 +1,9 @@
-import React, { useState, useEffect  } from 'react';
+import React, { useState } from 'react';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import './animation.css';
-import { doc, setDoc } from "firebase/firestore"; 
+import { doc, setDoc } from 'firebase/firestore'; 
 import { db } from '../firebase';
-
 
 export default function SignIn() {
   const [displayName, setDisplayName] = useState('');
@@ -16,27 +15,26 @@ export default function SignIn() {
     }
 
     const provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider);
-  };
-
-  useEffect(() => {
-    firebase.auth().onAuthStateChanged(user => {
-      if (user) {
+    firebase.auth().signInWithPopup(provider)
+      .then((result) => {
+        const user = result.user;
         const userId = user.uid;
-         setDoc(doc(db, "users", userId), {
+        setDoc(doc(db, 'users', userId), {
           displayName,
           userId,
         });
-      }
-    });
-  }, );
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   return (
     <div className='flex items-center justify-center bg-gray-900 min-h-screen min-w-screen'>
       <div className='bg-cyan-500 p-16 rounded-full transition-transform duration-500 animate-scale px-24'>
         <div className='transition-transform duration-500 animate-scale1'>
           <h1 className='text-white select-none text-[75px] font-bold font-mono underline decoration-dashed'>ChatApp</h1>
-          <div className="flex flex-col items-center">
+          <div className='flex flex-col items-center'>
             <input
               type='text'
               placeholder='Enter display name'
@@ -47,7 +45,6 @@ export default function SignIn() {
             <button className='rounded-full p-2 border-whitesmoke text-xl border-4 border-solid bg-gray-700 text-white font-mono' onClick={signInWithGoogle}>
               Sign In with Google
             </button>
-
           </div>
         </div>
       </div>
