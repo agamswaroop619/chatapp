bf925bc868159908bd40870207efcd064f1cae08
agamswaroop619 <agam.swaroop619@gmail.com>
2023-06-11 17:04:02 +0530

    "SignIn issue resolved"

--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -1,30 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import './animation.css';
 
 export default function SignIn() {
-    let popupcounter=1;
+  const [displayName, setDisplayName] = useState('');
 
   const signInWithGoogle = () => {
-    if (popupcounter === 1) {
-      const provider = new firebase.auth.GoogleAuthProvider();
-      firebase.auth().signInWithPopup(provider);
-      popupcounter = popupcounter+1;
+    if (displayName.trim() === '') {
+      alert('Please enter a display name.');
+      return;
     }
-    else{
-      alert("SignIn Popup already open!");
-    }
-  };
 
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider);
+  };
 
   return (
     <div className='flex items-center justify-center bg-gray-900 min-h-screen min-w-screen'>
-      <div className='bg-cyan-500 p-16 rounded-full transition-transform duration-500 animate-scale'>
+      <div className='bg-cyan-500 p-16 rounded-full transition-transform duration-500 animate-scale px-24'>
         <div className='transition-transform duration-500 animate-scale1'>
-        <h1 className='text-white select-none text-[75px] font-bold font-mono underline decoration-dashed'>ChatApp</h1>
-        <button className='rounded-full p-2 border-whitesmoke text-xl border-4 border-solid bg-gray-700 text-white font-mono' onClick={signInWithGoogle}>Sign In with Google</button>
-      </div>
+          <h1 className='text-white select-none text-[75px] font-bold font-mono underline decoration-dashed'>ChatApp</h1>
+          <div className="flex flex-col items-center">
+            <input
+              type='text'
+              placeholder='Enter display name'
+              value={displayName}
+              onChange={(e) => setDisplayName(e.target.value)}
+              className='rounded-full p-2 border-whitesmoke text-xl border-4 border-solid bg-gray-700 text-white font-mono mb-1'
+            />
+            <button className='rounded-full p-2 border-whitesmoke text-xl border-4 border-solid bg-gray-700 text-white font-mono' onClick={signInWithGoogle}>
+              Sign In with Google
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
