5d2944460fefbcdff5d97d754f58cee2cdf63a2d
agamswaroop619 <agam.swaroop619@gmail.com>
2023-06-11 16:49:43 +0530

    "final UI improvements"

--- src/Components/ChatRoom/Message.jsx
+++ src/Components/ChatRoom/Message.jsx
@@ -3,7 +3,17 @@ import React, { Component } from 'react'
 export class Message extends Component {
   render() {
     return (
-      <div>Message</div>
+      <div className='flex flwx-col gap-[20px]'>
+        <div id='messageInfo'>
+        <img className='h-[50px] w-[50px] rounded-full' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvzC4vcQZWXEYl5FBNhRu75i-zcY3gfT5DAWhSgZVbFQ&s'
+        alt='Aarat'
+        />
+        <span className='text-gray-500'>Just now</span>
+        </div>
+        <div id='messageContent' className='max-w-[80%] flex flex-col gap-[10px]'>
+        <p className='bg-white rounded-full rounded-tl-none px-5'>Hello</p>
+        </div>
+      </div>
     )
   }
 }
