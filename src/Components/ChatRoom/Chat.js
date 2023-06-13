f0b52eda16745de9bec4f2793cfdd1ee1ccd0f8d
agamswaroop619 <agam.swaroop619@gmail.com>
2023-06-14 03:14:15 +0530

    userChats working

--- src/Components/ChatRoom/Chat.js
+++ src/Components/ChatRoom/Chat.js
@@ -1,19 +1,29 @@
 import React, { useContext, useState } from 'react';
 import { db } from '../../firebase';
-import { collection, query, where, getDocs, getDoc, doc, setDoc, serverTimestamp, updateDoc } from 'firebase/firestore';
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  getDoc,
+  doc,
+  setDoc,
+  serverTimestamp,
+  updateDoc
+} from 'firebase/firestore';
 import { AuthContext } from '../Context/AuthContext';
 import Topnav from './Topnav';
 import Chatlog from './Chatlog';
 
 const Chat = () => {
-  const [username, setUsername] = useState("");
+  const [username, setUsername] = useState('');
   const [user, setUser] = useState(null);
   const [err, setErr] = useState(false);
 
   const { currentUser } = useContext(AuthContext);
 
   const handleSearch = async () => {
-    const q = query(collection(db, "users"), where("displayName", "==", username));
+    const q = query(collection(db, 'users'), where('displayName', '==', username));
 
     try {
       const querySnapshot = await getDocs(q);
@@ -21,55 +31,67 @@ const Chat = () => {
         setUser(doc.data());
       });
     } catch (error) {
-      console.error("Error searching for user:", error);
+      console.error('Error searching for user:', error);
       setErr(true);
     }
   };
 
   const handleSelect = async () => {
+    if (!user) {
+      return;
+    }
+
     const combinedId =
-      currentUser.uid > user.userId
-        ? currentUser.uid + user.userId
-        : user.userId + currentUser.uid; 
-  
+      currentUser.uid > user.userId ? currentUser.uid + user.userId : user.userId + currentUser.uid;
+
     try {
-      const docRef = doc(db, "chats", combinedId);
+      const docRef = doc(db, 'chats', combinedId);
       const docSnapshot = await getDoc(docRef);
-  
+
       if (!docSnapshot.exists()) {
         await setDoc(docRef, { messages: [] });
       }
-  
-      const currentUserChatRef = doc(db, "userChats", currentUser.uid);
-      const userChatRef = doc(db, "userChats", user.uid);
-  
-      await updateDoc(userChatRef, {
-        [combinedId]: {
-          userInfo: {
-            uid: currentUser.uid,
-            displayName: currentUser.displayName,
-          },
-          date: serverTimestamp(),
+
+      const userChatRef = doc(db, 'userChats', currentUser.uid);
+      const otherUserChatRef = doc(db, 'userChats', user.userId);
+
+      const userChatSnapshot = await getDoc(userChatRef);
+      const otherUserChatSnapshot = await getDoc(otherUserChatRef);
+
+      const userChatData = {
+        userInfo: {
+          uid: user.userId,
+          displayName: user.displayName
         },
-      });
-  
-      await updateDoc(currentUserChatRef, {
-        [combinedId]: {
-          userInfo: {
-            uid: user.userId,
-            displayName: user.displayName,
-          },
-          date: serverTimestamp(),
+        date: serverTimestamp()
+      };
+
+      const otherUserChatData = {
+        userInfo: {
+          uid: currentUser.uid,
+          displayName: currentUser.displayName
         },
-      });
-    } catch (err) {
-      console.error("Error handling user selection:", err);
+        date: serverTimestamp()
+      };
+
+      if (!userChatSnapshot.exists()) {
+        await setDoc(userChatRef, { [combinedId]: userChatData });
+      } else {
+        await updateDoc(userChatRef, { [combinedId]: userChatData });
+      }
+
+      if (!otherUserChatSnapshot.exists()) {
+        await setDoc(otherUserChatRef, { [combinedId]: otherUserChatData });
+      } else {
+        await updateDoc(otherUserChatRef, { [combinedId]: otherUserChatData });
+      }
+    } catch (error) {
+      console.error('Error handling user selection:', error);
     }
   };
-  
 
   const handleKey = (e) => {
-    if (e.code === "Enter") {
+    if (e.code === 'Enter') {
       handleSearch();
     }
   };
@@ -82,6 +104,7 @@ const Chat = () => {
           className='w-full bg-transparent shadow-[inset_0px_-3px_0px_0px_rgb(255,255,255,0.3)] font-mono text-white'
           type='text'
           placeholder='Find Your Chat!'
+          value={username}
           onChange={(e) => setUsername(e.target.value)}
           onKeyDown={handleKey}
         />
